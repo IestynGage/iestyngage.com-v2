3a49a2b3e625274613406153510f7beb50625da3
Iestyn <iestyngage@Fancy-Abacus.local>
2022-11-23 18:48:18 +0000

    Get 11ty to generate site map

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/assets");
-  eleventyConfig.addPassthroughCopy({"./src/robots":"/"});
+  eleventyConfig.addPassthroughCopy({"./src/robots/robots.txt":"robots.txt"});
 
   return {
     dir: {
